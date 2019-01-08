ad62bf3369b5927a4e1707526c072efd09cc8b1c
wangxiaoqing <xiaoqingwang@126.com>
2019-01-08 16:35:38 +0800

    add

--- pcapp/src/env/config.dev.js
+++ pcapp/src/env/config.dev.js
@@ -1,5 +1,5 @@
-let islocalhost = true;
-const serverurl = islocalhost?'http://localhost:6012':'http://192.168.1.51:6012';
+let islocalhost = false;
+const serverurl = islocalhost?'http://localhost:6012':'http://yt.i2u.top:6012';
 
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/api`;
 const wspath = islocalhost?'/socket.io':'/socket.io';
--- pcapp/src/env/config.prod.js
+++ pcapp/src/env/config.prod.js
@@ -1,5 +1,5 @@
 let islocalhost = false;
-const serverurl = islocalhost?'http://localhost:6012':'http://192.168.1.51:6012';
+const serverurl = islocalhost?'http://localhost:6012':'http://yt.i2u.top:6012';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/api`;
 const wspath = islocalhost?'/socket.io':'/socket.io';
 
